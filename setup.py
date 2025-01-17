57c523f6e5f63e32c5e5400e590d7bdb3cb3325d
Vacek-Ace <rodetlas@gmail.com>
2025-01-17 13:21:26 +0100

    <CHORE> Update project configuration files
    - Added MANIFEST.in to include additional files (README, LICENSE, data, and images) in the package distribution.
    - Updated requirements.txt with dependencies and editable package reference.
    - Modified setup.py to improve project metadata and ensure compatibility with the new structure.

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='dynamic_disagreeing_neighbors',
     packages=find_packages(),
     version='0.1.0',
-    description='Dynamic Disagreeing Neighbors',
+    description='Dynamic Disagreeing Neighbors: A complexity measure for classification.',
     author='Víctor Aceña',
     license='GPL-3.0',
+    include_package_data=True,
 )
